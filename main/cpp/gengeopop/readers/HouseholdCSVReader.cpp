94a11c9a4f7f3cd29ec994a5ee08f831fa467c8e
Thomas Avé <email@thomasave.be>
2018-03-10 13:13:43 +0100

    Don't copy-construct

--- main/cpp/gengeopop/readers/HouseholdCSVReader.cpp
+++ main/cpp/gengeopop/readers/HouseholdCSVReader.cpp
@@ -5,21 +5,21 @@ gengeopop::HouseholdCSVReader::HouseholdCSVReader(std::istream &inputStream) {
     stride::util::CSV reader(inputStream);
 
     for (const stride::util::CSVRow &row : reader) {
-        gengeopop::Household household;
+        std::shared_ptr<gengeopop::Household> household = std::make_shared<gengeopop::Household>();
 
         // Create contactpool of the household
-        ContactPool newCP = ContactPool();
+        std::shared_ptr<ContactPool> newCP = std::make_shared<ContactPool>();
         for (int i = 0; i < 12; i++) {
             std::string ageString = row.getValue(i);
             if (ageString == "NA") {
                 break;
             }
             int age = stoi(ageString);
-            stride::Person p = stride::Person();
-            p.setAge(age);
-            newCP.addMember(std::make_shared<stride::Person>(p));
+            std::shared_ptr<stride::Person> p = std::make_shared<stride::Person>();
+            p->setAge(age);
+            newCP->addMember(p);
         }
-        household.addPool(std::make_shared<ContactPool>(newCP));
-        m_households.push_back(std::make_shared<Household>(household));
+        household->addPool(newCP);
+        m_households.push_back(household);
     }
 }
