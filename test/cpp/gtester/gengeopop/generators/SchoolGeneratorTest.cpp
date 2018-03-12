18512e2235ca658fe730be3e3eeef954e378547e
Thomas Avé <email@thomasave.be>
2018-03-12 17:10:45 +0100

    Fix test after changing Location constructor

--- test/cpp/gtester/gengeopop/generators/SchoolGeneratorTest.cpp
+++ test/cpp/gtester/gengeopop/generators/SchoolGeneratorTest.cpp
@@ -19,7 +19,7 @@ namespace {
             config.fraction_compulsoryPupils = 0.20;
 
             auto geoGrid = std::make_shared<GeoGrid>();
-            auto loc1 = std::make_shared<Location>(1, 4, 2500, 0, 0, 0, 0, "Antwerpen");
+            auto loc1 = std::make_shared<Location>(1, 4, 2500, Coordinate(0, 0, 0, 0), "Antwerpen");
             geoGrid->addLocation(loc1);
 
             schoolGenerator.apply(geoGrid, config);
@@ -57,11 +57,11 @@ namespace {
                 config.fraction_compulsoryPupils = 0.20;
 
                 auto geoGrid = std::make_shared<GeoGrid>();
-                auto loc1 = std::make_shared<Location>(1, 4, 10150*100, 0, 0, 0, 0, "Antwerpen");
-                auto loc2 = std::make_shared<Location>(1, 4, 10040*100, 0, 0, 0, 0, "Vlaams-Brabant");
-                auto loc3 = std::make_shared<Location>(1, 4, 7460*100, 0, 0, 0, 0, "Henegouwen");
-                auto loc4 = std::make_shared<Location>(1, 4, 3269*100, 0, 0, 0, 0, "Limburg");
-                auto loc5 = std::make_shared<Location>(1, 4, 4123*100, 0, 0, 0, 0, "Luxemburg");
+                auto loc1 = std::make_shared<Location>(1, 4, 10150*100, Coordinate(0, 0, 0, 0), "Antwerpen");
+                auto loc2 = std::make_shared<Location>(1, 4, 10040*100, Coordinate(0, 0, 0, 0), "Vlaams-Brabant");
+                auto loc3 = std::make_shared<Location>(1, 4, 7460*100, Coordinate(0, 0, 0, 0), "Henegouwen");
+                auto loc4 = std::make_shared<Location>(1, 4, 3269*100, Coordinate(0, 0, 0, 0), "Limburg");
+                auto loc5 = std::make_shared<Location>(1, 4, 4123*100, Coordinate(0, 0, 0, 0), "Luxemburg");
 
                 geoGrid->addLocation(loc1);
                 geoGrid->addLocation(loc2);
