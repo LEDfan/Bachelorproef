c56498a11d4ef7019e6d3376bb163b50f6efb7d8
Robin Jadoul <Robin.Jadoul@student.uantwerpen.be>
2018-03-17 11:57:13 +0100

    Save work on k-d tree

--- main/cpp/gengeopop/KdTree.h
+++ main/cpp/gengeopop/KdTree.h
@@ -2,7 +2,17 @@
 
 #include <cstddef>
 #include <functional>
+#include <memory>
 #include <vector>
+
+namespace {
+class BaseNode;
+template <typename P, std::size_t D> class Node;
+} // namespace
+
+/**********************************
+*  Public interface starts here  *
+**********************************/
 
 namespace gengeopop {
 
@@ -24,7 +34,8 @@ struct AABB
  *
  * The template parameter `P` should have the following attributes and operations:
  *  - A `static constexpr std::size_t dim`: the number of dimensions of the point type.
- *  - A `template <std::size_t d> get()` method that returns the coordinate of the `d`th dimension of the point
+ *  - A `get(std::size_t d)` method that returns the coordinate of the `d`th dimension of the point
+ *  - The dimensions should have a total order
  */
 template <typename P>
 class KdTree
@@ -33,7 +44,7 @@ public:
         /**
          * Constructor: builds an empty tree
          */
-        KdTree();
+        KdTree() : m_size(0), m_root(nullptr) {}
 
         /**
          * Build a balanced tree from the given set of points efficiently
@@ -41,22 +52,35 @@ public:
          * @param points The points to insert in the resulting tree
          * @returns A balanced KdTree containing the given points
          */
-        static KdTree Build(const std::vector<P>& points);
+        static KdTree Build(const std::vector<P>& points) {
+                //TODO
+        }
 
         /**
          * Insert a new point into the tree, using this often may result in an unbalanced tree
          *
          * @param point The point to insert into the tree
          */
-        void Insert(P point);
+        void Insert(P point) {
+                //TODO
+        }
 
         /**
          * Test wether a point is contained in the tree
          *
-         * @param point The point to test
+         * @param point The point to test. P should support `bool operator==(const P&) const`
          * @returns Whether the point is found in the tree
          */
-        bool Contains(P point);
+        bool Contains(P point) const {
+                bool result = false;
+                Apply([&result, &point](const P& pt) -> bool {
+                        if (pt == point) {
+                                result = true;
+                                return false;
+                        }
+                        return true;
+                });
+        }
 
         /**
          * Get all points in the tree that lie within `box`
@@ -64,22 +88,92 @@ public:
          * @param box The limiting AABB to search for points
          * @returns A collection of points found within `box`
          */
-        std::vector<P> Query(const AABB<P>& box);
+        std::vector<P> Query(const AABB<P>& box) const {
+                std::vector<P> result;
+                Apply([&result](const P& pt) -> bool {
+                        result.push_back(pt);
+                }, box);
+                return result;
+        }
 
         /**
          * Calls a function with each of the points in the tree
          *
-         * @param f A function that will be called with each point
+         * @param f A function that will be called with each point, if f returns false, the traversal stops
          */
-        void Apply(std::function<void(const P&)> f);
+        void Apply(std::function<bool(const P&)> f) const;
 
         /**
          * Calls a function with every point contained in `box`
          *
-         * @param f A function that will be called with each point within `box`
+         * @param f A function that will be called with each point within `box`, if f returns false, the traversal stops
          * @param box The containing Axis-Aligned Bounding Box to search for points
          */
-        void Apply(std::function<void(const P&)> f, const AABB<P>& box);
+        void Apply(std::function<bool(const P&)> f, const AABB<P>& box) const;
+
+        /**
+         * Get the height of the tree
+         *
+         * Mostly for testing purposes
+         */
+        std::size_t Height() const;
+
+        /**
+         * Is the tree empty
+         */
+        bool Empty() const;
+
+        /**
+         * Get the size of the tree
+         */
+        std::size_t Size() const;
+
+private:
+
+        std::size_t m_size; ///< The number of points in the tree
+        std::unique_ptr<Node<P, 0>> m_root; ///< The root node of the tree
 };
 
 } // namespace gengeopop
+
+
+
+/***************************************
+*  Implementation details start here  *
+***************************************/
+namespace {
+
+class BaseNode {
+        virtual ~BaseNode() {};
+
+        /**
+         * Get a non-owning pointer to the left child
+         * nullptr if there's no such child
+         */
+        virtual BaseNode* BorrowLeft() = 0;
+
+        /**
+         * Get a non-owning pointer to the right child
+         * nullptr if there's no such child
+         */
+        virtual BaseNode* BorrowRight() = 0;
+};
+
+/**
+ * A node in the KdTree
+ *
+ * Template parameter P: the type of point
+ * Template parameter D: The dimension this node splits on
+ */
+template <typename P, std::size_t D>
+class Node : public BaseNode {
+        P point;
+        std::unique_ptr<Node<P, (D + 1) % P::dim>> left, right;
+
+        Node(P pt) : point(pt) {}
+
+        BaseNode* BorrowLeft() override;
+        BaseNode* BorrowRight() override;
+};
+
+} // namespace
