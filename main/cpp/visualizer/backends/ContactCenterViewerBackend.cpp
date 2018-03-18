80d4e00f666f4bea7ac0a0246819c650d0ea192c
NielsAerens <t3ch0wn3r@gmail.com>
2018-03-18 18:39:41 +0100

    Remove unneeded text

--- main/cpp/visualizer/backends/ContactCenterViewerBackend.cpp
+++ main/cpp/visualizer/backends/ContactCenterViewerBackend.cpp
@@ -5,15 +5,12 @@
 
 void ContactCenterViewerBackend::showContactCenter(std::shared_ptr<gengeopop::ContactCenter> center)
 {
-        QObject* nameText        = parent()->findChild<QObject*>(QString("textCCID"));
         QObject* poolSizeText    = parent()->findChild<QObject*>(QString("textCCPoolSize"));
         QObject* maxPoolSizeText = parent()->findChild<QObject*>(QString("textCCMaxPools"));
 
-        QString idString       = "ID: " + QString::fromStdString("0");
         QString poolsString    = "Pool Size: " + QString::number(center->getPoolSize());
         QString maxPoolsString = "Max # Pools: " + QString::number(center->getMaxPools());
 
-        nameText->setProperty("text", idString);
         poolSizeText->setProperty("text", poolsString);
         maxPoolSizeText->setProperty("text", maxPoolsString);
 }
