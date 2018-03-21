fce994ee6385f4261c6f147bb6f63605ffe891c3
Thomas Avé <email@thomasave.be>
2018-03-21 17:13:03 +0100

    Parallize GeoGridReader + fix its tests

--- main/cpp/gengeopop/io/GeoGridJSONReader.cpp
+++ main/cpp/gengeopop/io/GeoGridJSONReader.cpp
@@ -24,19 +24,40 @@ std::shared_ptr<GeoGrid> GeoGridJSONReader::read(std::istream& stream)
                     "There was a problem parsing the JSON file, please check if it is not empty and it is valid JSON.");
         }
         auto geoGrid = std::make_shared<GeoGrid>();
-        auto people  = root.get_child("persons");
-        for (auto it = people.begin(); it != people.end(); it++) {
-                auto person               = ParsePerson(it->second.get_child(""));
-                m_people[person->GetId()] = person;
-        }
 
+        auto people = root.get_child("persons");
+#pragma omp parallel
+#pragma omp single
+        {
+                for (auto it = people.begin(); it != people.end(); it++) {
+                        std::shared_ptr<stride::Person> person;
+#pragma omp task firstprivate(it)
+                        {
+                                person = ParsePerson(it->second.get_child(""));
+#pragma omp critical
+                                m_people[person->GetId()] = std::move(person);
+                        }
+                }
+#pragma omp taskwait
+        }
         auto locations = root.get_child("locations");
-        for (auto it = locations.begin(); it != locations.end(); it++) {
-                geoGrid->addLocation(ParseLocation(it->second.get_child("")));
+#pragma omp parallel
+#pragma omp single
+        {
+                for (auto it = locations.begin(); it != locations.end(); it++) {
+                        std::shared_ptr<Location> loc;
+#pragma omp task firstprivate(it)
+                        {
+                                loc = ParseLocation(it->second.get_child(""));
+#pragma omp critical
+                                geoGrid->addLocation(std::move(loc));
+                        }
+                }
+#pragma omp taskwait
         }
         m_people.clear();
         return geoGrid;
-}
+} // namespace gengeopop
 
 std::shared_ptr<Location> GeoGridJSONReader::ParseLocation(boost::property_tree::ptree& location)
 {
@@ -50,8 +71,19 @@ std::shared_ptr<Location> GeoGridJSONReader::ParseLocation(boost::property_tree:
 
         auto contactCenters = location.get_child("contactCenters");
 
-        for (auto it = contactCenters.begin(); it != contactCenters.end(); it++) {
-                result->addContactCenter(ParseContactCenter(it->second.get_child("")));
+#pragma omp parallel
+#pragma omp single
+        {
+                for (auto it = contactCenters.begin(); it != contactCenters.end(); it++) {
+                        std::shared_ptr<ContactCenter> center;
+#pragma omp task firstprivate(it)
+                        {
+                                center = ParseContactCenter(it->second.get_child(""));
+#pragma omp critical
+                                result->addContactCenter(center);
+                        }
+                }
+#pragma omp taskwait
         }
         return result;
 }
@@ -84,17 +116,31 @@ std::shared_ptr<ContactCenter> GeoGridJSONReader::ParseContactCenter(boost::prop
         }
 
         auto contactPools = contactCenter.get_child("pools");
-        for (auto it = contactPools.begin(); it != contactPools.end(); it++) {
-                result->addPool(ParseContactPool(it->second.get_child("")));
+
+#pragma omp parallel
+#pragma omp single
+        {
+                for (auto it = contactPools.begin(); it != contactPools.end(); it++) {
+                        std::shared_ptr<ContactPool> pool;
+#pragma omp task firstprivate(it)
+                        {
+                                pool = ParseContactPool(it->second.get_child(""));
+#pragma omp critical
+                                result->addPool(pool);
+                        }
+                }
+#pragma omp taskwait
         }
+
         return result;
 }
 
 std::shared_ptr<ContactPool> GeoGridJSONReader::ParseContactPool(boost::property_tree::ptree& contactPool)
 {
-        auto id     = boost::lexical_cast<unsigned int>(contactPool.get<std::string>("id"));
-        auto result = std::make_shared<ContactPool>(id);
-        auto people = contactPool.get_child("people");
+        unsigned int id     = boost::lexical_cast<unsigned int>(contactPool.get<std::string>("id"));
+        auto         result = std::make_shared<ContactPool>(id);
+        auto         people = contactPool.get_child("people");
+
         for (auto it = people.begin(); it != people.end(); it++) {
                 auto person_id = boost::lexical_cast<unsigned int>(it->second.get<std::string>(""));
                 if (m_people.count(person_id) == 0) {
