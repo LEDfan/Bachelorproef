23c218bbddd21f184e6a0dbaef0c8d634c85d8ec
Robin Jadoul <Robin.Jadoul@student.uantwerpen.be>
2018-04-09 17:30:15 +0200

    Clean ThreadException::Run

--- main/cpp/gengeopop/io/ThreadException.h
+++ main/cpp/gengeopop/io/ThreadException.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <boost/optional.hpp>
 #include <mutex>
 
 namespace gengeopop {
+
 
 // Based on https://stackoverflow.com/a/13978507/1393103
 class ThreadException
@@ -22,14 +24,23 @@ public:
                 this->ptr = std::current_exception();
         }
 
+        template <typename T>
+        struct RunType {
+                using type = boost::optional<T>;
+        };
+
+        template <typename T>
+        using RunType_t = typename RunType<T>::type;
+
         template <typename Function, typename... Parameters>
-        void Run(Function f, Parameters... params)
+        auto Run(Function f, Parameters... params)
         {
                 try {
-                        f(params...);
+                        return f(params...);
                 } catch (...) {
                         CaptureException();
                 }
+                return RunType_t<std::result_of_t<Function(Parameters...)>>();
         }
 
         bool HasError() const { return ptr != nullptr; }
@@ -37,6 +48,11 @@ public:
 private:
         std::exception_ptr ptr;
         std::mutex         Lock;
+};
+
+template <>
+struct ThreadException::RunType<void> {
+        using type = void;
 };
 
 } // namespace gengeopop
