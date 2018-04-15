bc797c110e35af9f521560d7deacad407fdf11c6
NielsAerens <t3ch0wn3r@gmail.com>
2018-04-15 17:29:55 +0200

    Comment main.cpp

--- main/cpp/visualizer/main.cpp
+++ main/cpp/visualizer/main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char* argv[])
 {
         QGuiApplication app(argc, argv);
 
+        // Register our custom components
         qmlRegisterType<Backend>("io.bistromatics.backend", 1, 0, "Backend");
         qmlRegisterType<LocationViewerBackend>("io.bistromatics.locationviewerbackend", 1, 0, "LocationViewerBackend");
         qmlRegisterType<ContactCenterViewerBackend>("io.bistromatics.contactcenterviewerbackend", 1, 0,
@@ -22,15 +23,19 @@ int main(int argc, char* argv[])
         qmlRegisterType<ContactCenterListModel>("io.bistromatics.contactcenterlistmodel", 1, 0,
                                                 "ContactCenterListModel");
 
+        // Register the custom types that are not standard to QML
         qRegisterMetaType<std::shared_ptr<gengeopop::Location>>();
         qRegisterMetaType<std::vector<std::shared_ptr<gengeopop::ContactCenter>>>();
         qRegisterMetaType<std::set<std::shared_ptr<gengeopop::Location>>>();
         QQmlApplicationEngine engine;
 
+        // Create and expose the models
         ContactCenterListModel ccModel;
         ContactPoolListModel   cpModel;
         engine.rootContext()->setContextProperty("ccModel", &ccModel);
         engine.rootContext()->setContextProperty("cpModel", &cpModel);
+
+        // Load the main QML
         engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
         if (engine.rootObjects().isEmpty())
                 return -1;
