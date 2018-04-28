68481a5983e52fc834cf4463e1720db0013c6a10
Thomas Avé <email@thomasave.be>
2018-04-28 15:00:03 +0200

    More documentation

--- main/cpp/calibration/Boxplot.h
+++ main/cpp/calibration/Boxplot.h
@@ -21,7 +21,9 @@ public:
         void WriteToFile();
 
 private:
-        double FindMedian(unsigned long begin, unsigned long end, std::string testcase);
+        double FindMedian(
+            unsigned long begin, unsigned long end,
+            std::string testcase);      ///< Find the median of values between begin and end in the results[testcase]
         void GeneratePlots(bool write); ///< Generate the actual plots. Write them to files if write is true, otherwise
                                         ///< display them on screen.
         std::map<std::string, std::vector<unsigned int>> results; ///< Storage for the simulation results
