870ba755224cc6b73d7dd58253414828b0d34b12
Thomas Avé <email@thomasave.be>
2018-04-28 15:53:26 +0200

    Constness 🎉

--- main/cpp/gengeopop/populators/PrimaryCommunityPopulator.cpp
+++ main/cpp/gengeopop/populators/PrimaryCommunityPopulator.cpp
@@ -19,15 +19,15 @@ void PrimaryCommunityPopulator::Apply(std::shared_ptr<GeoGrid> geoGrid, GeoGridC
         // for every location
         for (const std::shared_ptr<Location>& loc : *geoGrid) {
                 // 1. find all communities in an area of 10-k*10 km
-                auto community_pools = GetContactPoolInIncreasingRadius<PrimaryCommunity>(geoGrid, loc);
+                const auto& community_pools = GetContactPoolInIncreasingRadius<PrimaryCommunity>(geoGrid, loc);
 
                 // 2. for every household assign a community
-                auto dist = m_rnManager.GetGenerator(trng::uniform_int_dist(
+                const auto dist = m_rnManager.GetGenerator(trng::uniform_int_dist(
                     0, static_cast<trng::uniform_int_dist::result_type>(community_pools.size())));
 
                 for (const std::shared_ptr<ContactCenter>& household : loc->GetContactCentersOfType<Household>()) {
-                        const std::shared_ptr<ContactPool> contactPool = household->GetPools()[0];
-                        auto                               pool        = dist();
+                        const std::shared_ptr<ContactPool>& contactPool = household->GetPools()[0];
+                        const auto                          pool        = dist();
                         for (stride::Person* person : *contactPool) {
                                 found.insert(community_pools[pool]);
                                 if (community_pools[pool].get() == nullptr) {
--- main/cpp/gengeopop/populators/SecondaryCommunityPopulator.cpp
+++ main/cpp/gengeopop/populators/SecondaryCommunityPopulator.cpp
@@ -19,16 +19,16 @@ void SecondaryCommunityPopulator::Apply(std::shared_ptr<GeoGrid> geoGrid, GeoGri
         // for every location
         for (const std::shared_ptr<Location>& loc : *geoGrid) {
                 // 1. find all communities in an area of 10-k*10 km
-                auto community_pools = GetContactPoolInIncreasingRadius<SecondaryCommunity>(geoGrid, loc);
+                const auto& community_pools = GetContactPoolInIncreasingRadius<SecondaryCommunity>(geoGrid, loc);
 
                 // 2. for every household assign a community
-                auto dist = m_rnManager.GetGenerator(trng::uniform_int_dist(
+                const auto dist = m_rnManager.GetGenerator(trng::uniform_int_dist(
                     0, static_cast<trng::uniform_int_dist::result_type>(community_pools.size())));
 
                 for (const std::shared_ptr<ContactCenter>& household : loc->GetContactCentersOfType<Household>()) {
-                        const std::shared_ptr<ContactPool> contactPool = household->GetPools()[0];
+                        const std::shared_ptr<ContactPool>& contactPool = household->GetPools()[0];
                         for (stride::Person* person : *contactPool) {
-                                auto pool = dist();
+                                const auto pool = dist();
                                 found.insert(community_pools[pool]);
                                 community_pools[pool]->AddMember(person);
                         }
