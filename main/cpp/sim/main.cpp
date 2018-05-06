afcf55606ca79cb585bc25e6a2f8dd53bf6434ad
Tobia De Koninck <LEDfan@users.noreply.github.com>
2018-05-06 13:23:37 +0200

    Small fixes

--- main/cpp/sim/main.cpp
+++ main/cpp/sim/main.cpp
@@ -120,7 +120,6 @@ int main(int argc, char** argv)
                                 controller = std::make_shared<CliController>(configPt);
                         }
                         controller->Control();
-                        CliController(configPt).Control();
                 }
                 // -----------------------------------------------------------------------------------------
                 // If run simulation in gui ...
