e14ce108b910d3ddbfc21af3cf18d3588cd104c0
Niels Aerens <t3ch0wn3r@gmail.com>
2018-05-21 17:45:19 +0200

    interim

--- test/cpp/gtester/RegionSlicer.cpp
+++ test/cpp/gtester/RegionSlicer.cpp
@@ -45,17 +45,40 @@ TEST(RegionSlicer, Basic)
         std::shared_ptr<stride::Population> pop = stride::Population::Create();
         pop->CreatePerson(0, 0, 10, 1, 1, 1, 1, 1);
         pop->CreatePerson(0, 1, 11, 2, 2, 2, 2, 2);
+        pop->CreatePerson(0, 1, 12, 1, 2, 1, 2, 1);
 
         ASSERT_EQ((*(pop->begin())).GetAge(), 10);
 
         pop->Finalize();
         RegioSlicer slicer(pop);
         slicer[stride::ContactPoolType::Id::Household];
-        int i = 10;
-        for (auto it = slicer.begin(); it != slicer.end(); it++) {
-                ASSERT_EQ(i, (*it)->GetAge());
-                i++;
-        }
+        auto it = slicer.begin();
+        // First Household
+        ASSERT_EQ(10, (*it++)->GetAge());
+        ASSERT_EQ(12, (*it++)->GetAge());
+        // Second Household
+        ASSERT_EQ(11, (*it++)->GetAge());
+        ASSERT_EQ(it, slicer.end());
 }
 
+TEST(RegionSlicer, MultiRegionContactpoolType)
+{
+        std::shared_ptr<stride::Population> pop = stride::Population::Create(2);
+        pop->CreatePerson(0, 0, 10, 1, 1, 1, 1, 1);
+        pop->CreatePerson(0, 1, 11, 2, 2, 2, 2, 2);
+        pop->CreatePerson(1, 1, 12, 1, 2, 1, 2, 1);
+
+        ASSERT_EQ((*(pop->begin())).GetAge(), 10);
+
+        pop->Finalize();
+        RegioSlicer slicer(pop);
+        slicer[stride::ContactPoolType::Id::Household];
+        auto it = slicer.begin();
+        // First Household
+        ASSERT_EQ(10, (*it++)->GetAge());
+        ASSERT_EQ(11, (*it++)->GetAge());
+        // Second Household
+        ASSERT_EQ(12, (*it++)->GetAge());
+        ASSERT_EQ(it, slicer.end());
+}
 } // namespace
