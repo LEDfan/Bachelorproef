59ebfec6fc44d37cb7072db8299dce9d518a9f7f
Tobia De Koninck <LEDfan@users.noreply.github.com>
2018-05-22 07:05:18 +0200

    Add ConcatenatedIterator

---
+++ main/cpp/util/IteratorPair.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace stride {
+namespace util {
+
+template <typename IteratorType>
+class IteratorPair
+{
+public:
+        IteratorPair(IteratorType begin, IteratorType end) : m_begin(begin), m_end(end) {}
+
+        IteratorType begin() { return m_begin; }
+
+        IteratorType end() { return m_end; }
+
+private:
+        IteratorType m_begin;
+        IteratorType m_end;
+};
+
+} // namespace util
+} // namespace stride
