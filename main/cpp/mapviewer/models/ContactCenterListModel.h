82a513f04395bfd5e470ecebdfc405bb23412cac
Niels Aerens <t3ch0wn3r@gmail.com>
2018-06-17 18:57:01 +0200

    Extra docs

--- main/cpp/mapviewer/models/ContactCenterListModel.h
+++ main/cpp/mapviewer/models/ContactCenterListModel.h
@@ -10,21 +10,51 @@ class ContactCenterListModel : public QAbstractListModel
         Q_OBJECT
 public:
         explicit ContactCenterListModel(QObject* parent = nullptr);
-        QVariant               data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-        int                    rowCount(const QModelIndex& parent = QModelIndex()) const override;
-        int                    columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
+        /**
+         * Returns the data at the given index and the given role.
+         * @param index The row we want data of
+         * @param role The role/column name we want the data of
+         */
+        QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+
+        /**
+         * Returns the amount of rows, this is the amount of
+         */
+        int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+
+        /**
+         * Returns the amount of columns, this is the amount of roles.
+         */
+        int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
+        /*
+         * Returns the available roles, the columns.
+         */
         QHash<int, QByteArray> roleNames() const override;
 
         Q_INVOKABLE
+        /**
+         * Sets the centers we want to show.
+         * @param locs The locations we want to show the contactCenters of.
+         */
         void SetCenters(std::set<std::shared_ptr<gengeopop::Location>> locs);
 
         Q_INVOKABLE
+        /**
+         * This function should be invoked when a row is to be clicked. It will then get the data necessary and emit a
+         * "ContactCenterSelected" signal with the corresponding ContactCenter.
+         */
         void HandleClickRow(unsigned int row);
 
 signals:
+        /**
+         * This signal is emmited when a Contact Center is selected. This will be after a HandleClickRow is called.
+         */
         void ContactCenterSelected(std::shared_ptr<gengeopop::ContactCenter> cc);
 
 private:
+        // The roles, This can be understood as the names of the (possible) collumns.
         enum Roles
         {
                 IDRole = Qt::UserRole + 1,
@@ -33,5 +63,6 @@ private:
                 MaxPools
         };
 
+        // The ContactCenters we are displaying
         std::vector<std::shared_ptr<gengeopop::ContactCenter>> m_centers;
 };
--- main/cpp/mapviewer/models/ContactPoolListModel.h
+++ main/cpp/mapviewer/models/ContactPoolListModel.h
@@ -9,12 +9,27 @@ class ContactPoolListModel : public QAbstractListModel
         Q_OBJECT
 public:
         ContactPoolListModel(QObject* parent = nullptr);
-        QVariant               data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-        int                    rowCount(const QModelIndex& parent = QModelIndex()) const override;
-        int                    columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
+        /**
+         * @param index the index we want to get the data of.
+         * @param role the role we want the data of
+         */
+        QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+
+        // Returns the amount of row, the amount of contactpools
+        int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+
+        // Returns the amount of columns
+        int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+
+        // Returns the available columns of the model
         QHash<int, QByteArray> roleNames() const override;
 
         Q_INVOKABLE
+        /**
+         * Sets the contactpool we want to show in the model.
+         * @param cc the contactecenters we want to show the ContactPools of.
+         */
         void SetPools(std::shared_ptr<gengeopop::ContactCenter> cc);
 
 private:
